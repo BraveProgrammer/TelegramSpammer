491136e225d69d7dbce444b2885081f8f72395e9
Brave Programmer <brave.programmer@yahoo.com>
2020-11-23 21:30:20 +0330

    fix bugs and add more features     - fix send text bugs     - update requirements     - add report command     - add join and leave feature     - add authenticator

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from telethon.tl.functions.channels import JoinChannelRequest, LeaveChannelRequest
+from telethon.tl.functions.messages import ReportRequest
 from telethon.sync import TelegramClient, events
+from telethon.tl.types import *
 from colorama import init, Fore, Back, Style
-import configparser, click, random
+import configparser, click, random, os
 from progress.bar import Bar
 
 config = configparser.ConfigParser()
@@ -8,37 +11,81 @@ config.read("config.ini")
 api_id = config["auth"]["api_id"]
 api_hash = config["auth"]["api_hash"]
 phone = config["auth"]["phone"]
+try: bot_token = config["auth"]["bot_token"]
+except: bot_token = None
+name = config["auth"]["name"]
 
-with TelegramClient("spammer", api_id, api_hash) as client:
-	init(autoreset=True)
-	client.start(phone)
-	@click.group()
-	def cli(): pass
-	@cli.command()
-	@click.argument("target")
-	@click.option("-n", "--count", default=1, help="Number of messages.")
-	@click.option("-t", "--text", help="Text.")
-	@click.option("-f", "--file", default=None, help="File.")
-	def send_text(target, count, text, file):
-		print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.WHITE} {'Text' if not file else 'File'}: {text if not file else file}, Target: {target}, Count: {count}")
-		if file: file = open(file, 'r').read().splitlines()
-		if not file:
-			bar = Bar("Processing", max=count)
-			for c in range(count):
-				client.send_message(target, text)
-				bar.next()
-			bar.finish()
-			print(f"\n{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.WHITE} Done.")
-		else:
-			c = 0
-			bar = Bar("Processing", max=count)
-			for line in file:
-				if c == count:
-					print(f"\n{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.WHITE} Done.")
-					break
-				client.send_message(target, line)
-				c += 1
-				bar.next()
-			bar.finish()
-	cli()
-	client.run_until_disconnected()
+client = TelegramClient(name, api_id, api_hash)
+init(autoreset=True)
+
+if bot_token: client.start(bot_token=bot_token)
+else: client.start(phone)
+
+@click.group()
+def cli(): pass
+
+@cli.command()
+@click.argument("target")
+@click.option("-c", "--count", type=int, default=1, help="Number of messages.")
+@click.option("-f", "--file", default=None, help="File.")
+def send_text(target, count, file):
+	print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.WHITE} {Fore.GREEN}File:{Fore.WHITE} {file}, {Fore.GREEN}Target:{Fore.WHITE} {target}, {Fore.GREEN}Count:{Fore.WHITE} {count}")
+	file = open(file, 'r').read().splitlines()
+	bar = Bar("Processing", fill='█', max=count)
+	i = 0
+	for c in range(count):
+		try:
+			client.send_message(target, file[i])
+			i += 1
+			bar.next()
+		except IndexError:
+			i = 0
+			client.send_message(target, file[i])
+			bar.next()
+	bar.finish()
+	print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.GREEN} Done.")
+
+@cli.command()
+def auth():
+	os.remove(f"{name}.session")
+	client = TelegramClient(name, api_id, api_hash)
+	if bot_token: client.start(bot_token=bot_token)
+	else: client.start(phone)
+	client.disconnect()
+	print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.GREEN} Done.")
+
+@cli.command()
+@click.argument("id")
+def join(id):
+	try:
+		client(JoinChannelRequest(id))
+		print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.GREEN} Joined.")
+	except: print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.RED} Can't Join.")
+
+@cli.command()
+@click.argument("id")
+def leave(id):
+	try:
+		client(LeaveChannelRequest(id))
+		print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.GREEN} Left.")
+	except: print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.RED} Can't Leave.")
+
+@cli.command()
+@click.argument("id")
+@click.argument("type", type=click.Choice(["porn", "spam", "violence", "other"]))
+@click.option("-c", "--count", type=int, default=1, help="Number of reports.")
+def report(id, type, count):
+	print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.WHITE} {Fore.GREEN}Type:{Fore.WHITE} {type}, {Fore.GREEN}Target:{Fore.WHITE} {id}, {Fore.GREEN}Count:{Fore.WHITE} {count}")
+	if type == "porn": reason = InputReportReasonPornography()
+	if type == "spam": reason = InputReportReasonPornography()
+	if type == "violence": reason = InputReportReasonViolence()
+	if type == "other": reason = InputReportReasonOther()
+	bar = Bar("Processing", fill='█', max=count)
+	for c in range(count):
+		client(ReportRequest(id, id=[42], reason=reason))
+		bar.next()
+	bar.finish()
+	print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.GREEN} Done.")
+
+cli()
+client.run_until_disconnected()
