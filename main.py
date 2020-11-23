56a47303899325fb4c9ff513586c88c241224fb0
Brave Programmer <brave.programmer@yahoo.com>
2020-11-23 16:59:37 +0330

    add text spammer     - update requirements     - add text spammer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from telethon.sync import TelegramClient, events
-import configparser
+from colorama import init, Fore, Back, Style
+import configparser, click, random
+from progress.bar import Bar
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -8,6 +10,35 @@ api_hash = config["auth"]["api_hash"]
 phone = config["auth"]["phone"]
 
 with TelegramClient("spammer", api_id, api_hash) as client:
+	init(autoreset=True)
 	client.start(phone)
-	client.send_message("me", "Hello, myself!")
+	@click.group()
+	def cli(): pass
+	@cli.command()
+	@click.argument("target")
+	@click.option("-n", "--count", default=1, help="Number of messages.")
+	@click.option("-t", "--text", help="Text.")
+	@click.option("-f", "--file", default=None, help="File.")
+	def send_text(target, count, text, file):
+		print(f"{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.WHITE} {'Text' if not file else 'File'}: {text if not file else file}, Target: {target}, Count: {count}")
+		if file: file = open(file, 'r').read().splitlines()
+		if not file:
+			bar = Bar("Processing", max=count)
+			for c in range(count):
+				client.send_message(target, text)
+				bar.next()
+			bar.finish()
+			print(f"\n{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.WHITE} Done.")
+		else:
+			c = 0
+			bar = Bar("Processing", max=count)
+			for line in file:
+				if c == count:
+					print(f"\n{Fore.BLUE}[{Fore.WHITE}*{Fore.BLUE}]{Fore.WHITE} Done.")
+					break
+				client.send_message(target, line)
+				c += 1
+				bar.next()
+			bar.finish()
+	cli()
 	client.run_until_disconnected()
